e324fe9912492465ea3029c78949f5cc2d58c3e1
vishal <vishaljagadale88@gmail.com>
2019-05-29 16:21:05 +0530

    myfile

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+//const bodyParser = require('body-parser');
+//const productRouter = require('./server');
+
+const app = express();
+
+
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded());
+//app.use(productRouter);
+
+app.listen(3434, '0.0.0.0', () => {
+    console.log('server started on port 3000');
+})
+  
